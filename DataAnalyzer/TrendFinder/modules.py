d6bfbe0711a85edd32a4217b9fd20fe9347707d2
William Starling <63762012+WilliamStarling@users.noreply.github.com>
2025-07-15 23:55:03 +0000

    began working on module for TrendFinder.

--- DataAnalyzer/TrendFinder/modules.py
+++ DataAnalyzer/TrendFinder/modules.py
@@ -2,7 +2,7 @@
 import dspy
 from attachments.dspy import Attachments
 
-class pdf_analyzer_sql(dspy.Signature):
+class doc_analyzer_sql(dspy.Signature):
   """You will receive a single document from a collection, and using the given categories you will generate a row of csv data that helps summarize the document."""
   document: Attachments = dspy.InputField(desc="the current document to analyze")
   categories: list[str] = dspy.InputField(desc="the categories to generate a datapoint for based on what's in the pdf.")
@@ -10,7 +10,7 @@ class pdf_analyzer_sql(dspy.Signature):
   next_context: str = dspy.OutputField(desc="The input context, where new information is optionally added on if thought to be important. New context, if present at all, should be brief to ensure the overall context doesn't get too long.")
   csv_row: str = dspy.OutputField(desc="a row of csv data that summarizes the document based on the given categories.")
   
-  class pdf_analyzer_nosql(dspy.Signature):
+  class doc_analyzer_nosql(dspy.Signature):
     """You will receive a single document from a collection, and using the given categories you will update the csv file to include the current documents's information."""
     document: Attachments = dspy.InputField(desc="the current document to analyze")
     categories: list[str] = dspy.InputField(desc="the categories to generate a datapoint for based on what's in the document.")
@@ -19,3 +19,12 @@ class pdf_analyzer_sql(dspy.Signature):
     next_context: str = dspy.OutputField(desc="The input context, where new information is optionally added on if thought to be important. New context, if present at all, should be brief to ensure the overall context doesn't get too long.")
     out_csv: str = dspy.OutputField(desc="The csv file updated to include the current document in it's information.")
 
+class collection_analyzer(dspy.Module):
+  def __init__(self):
+  super().__init__()
+self.doc_analyzer_sql = dspy.ChainOfThought(doc_analyzer_sql)
+self.doc_analyzer_nosql = dspy.ChainOfThought(doc_analyzer_nosql)
+
+def forward(self, documents: list[Attachments]):
+  for document in Attachments:
+  
