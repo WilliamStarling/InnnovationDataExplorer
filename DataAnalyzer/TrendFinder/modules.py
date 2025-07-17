9e4865d75215edfbbf6d628b3bd79796a8c079e1
William Starling <63762012+WilliamStarling@users.noreply.github.com>
2025-07-17 15:38:33 +0000

    Added test files and  started a program to test it. Also changed the code to only use one document analyzing signature that can both add onto a csv file or update one, depending one what it needs to do.

--- DataAnalyzer/TrendFinder/modules.py
+++ DataAnalyzer/TrendFinder/modules.py
@@ -1,30 +1,48 @@
 #For setting up and creating the DSPy signatures and modules needed to search for trends as a part of the bigger data analysis agent.
 import dspy
 from attachments.dspy import Attachments
+import pandas as pd
 
 # Print the dspy version being used
 print(f"DSPy version: {dspy.__version__}")
 
-class doc_analyzer_sql(dspy.Signature):
-  """You will receive a single document from a collection, and using the given categories you will generate a row of csv data that helps summarize the document."""
-  document: Attachments = dspy.InputField(desc="the current document to analyze")
-  categories: list[str] = dspy.InputField(desc="the categories to generate a datapoint for based on what's in the pdf.")
-  last_context: str = dspy.InputField(desc="Important context of the overall goal and previous steps from other agents.")
-  next_context: str = dspy.OutputField(desc="The input context, where new information is optionally added on if thought to be important. New context, if present at all, should be brief to ensure the overall context doesn't get too long.")
-  csv_row: str = dspy.OutputField(desc="a row of csv data that summarizes the document based on the given categories.")
-  
-  class doc_analyzer_nosql(dspy.Signature):
+class doc_analyzer(dspy.Signature):
   """You will receive a single document from a collection, and using the given categories you will update the csv file to include the current documents's information."""
-  document: Attachments = dspy.InputField(desc="the current document to analyze")
-  categories: list[str] = dspy.InputField(desc="the categories to generate a datapoint for based on what's in the document.")
-  in_csv: str = dspy.InputField(desc="The csv file being used to store information about the collection of documents.")
-  last_context: str = dspy.InputField(desc="Important context of the overall goal and previous steps from other agents.")
-  next_context: str = dspy.OutputField(desc="The input context, where new information is optionally added on if thought to be important. New context, if present at all, should be brief to ensure the overall context doesn't get too long.")
-  out_csv: str = dspy.OutputField(desc="The csv file updated to include the current document in it's information.")
+  document: Attachments = dspy.InputField(
+      desc="the current document to analyze")
+  categories: list[str] = dspy.InputField(
+      desc=
+      "the categories to generate a datapoint for based on what's in the document."
+  )
+  in_df: pd.DataFrame = dspy.InputField(
+      desc=
+      "The pandas dataframe being used to store information about the collection of documents."
+  )
+  last_context: str = dspy.InputField(
+      desc=
+      "Important context of the overall goal and previous steps from other agents."
+  )
+  next_context: str = dspy.OutputField(
+      desc=
+      "The input context, where new information is optionally added on if thought to be important. New context, if present at all, should be brief to ensure the overall context doesn't get too long."
+  )
+  out_df: str = dspy.OutputField(
+      desc=
+      "The pandas dataframe updated to include the current documents information."
+  )
 
-class collection_analyzer(dspy.Module):
+"""
+class trend_analyzer(dspy.Module):
   def __init__(self):
     super().__init__()
-    self.doc_analyzer_sql = dspy.ChainOfThought(doc_analyzer_sql)
-    self.doc_analyzer_nosql = dspy.ChainOfThought(doc_analyzer_nosql)
-  
+    self.doc_analyzer_sql = dspy.ChainOfThought(doc_analyzer)
+    
+  def forward(self, documents: list[Attachments], categories: list[str], context: str):
+    doc_summary = pd.DataFrame(columns=categories)
+    for document in documents:
+      result = self.doc_analyzer_sql(document=document, categories=categories, in_df = doc_summary, last_context=context)
+      context = result.next_context
+      doc_summary = result.out_csv
+      documents_csv = doc_summary.to_csv(index=False)
+    return document_csv, context
+"""
---
+++ DataAnalyzer/TrendFinder/test.py
@@ -0,0 +1,15 @@
+from modules import *
+
+documents = []
+documents.append(
+    Attachments(
+        "DataAnalyzer/TrendFinder/TrainingData/32037 ALR10C6BZ 097 07-10-2025 INSPR AEPACS NA.pdf"
+    ))
+documents.append(
+    Attachments(
+        "DataAnalyzer/TrendFinder/TrainingData/52781 ASN62022-CSW 097 07-10-2025 INSPR AEPACS Deer Crest.pdf"
+    ))
+documents.append(
+    Attachments(
+        "DataAnalyzer/TrendFinder/TrainingData/61763 ALR10C6PZ 077 07-10-2025 INSPR AEPACS NA.pdf"
+    ))
